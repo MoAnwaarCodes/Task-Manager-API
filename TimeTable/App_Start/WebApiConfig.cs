f5cd864a746be8805d6e5d09d417d29cc7adfa52
MoAnwaarCodes <moanwaar9@gmail.com>
2024-01-09 14:50:48 +0500

    Created three functions show data delete data and save data

---
+++ TimeTable/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace TimeTable
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TimeTable/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TimeTable.Models;
+namespace TimeTable.Controllers
+{
+    public class HomeController : ApiController
+    {
+       MyDataBaseEntities2 db=new MyDataBaseEntities2() ;
+        [HttpGet]
+        public HttpResponseMessage showData() {
+            return Request.CreateResponse(db.taskDetails);           
+        }
+        
+        [HttpGet]
+        public HttpResponseMessage saveData(string task) {
+            taskDetail t=new taskDetail();
+            t.Tasks = task;
+            db.taskDetails.Add(t);
+            db.SaveChanges();
+             return Request.CreateResponse("Successfully inserted");
+        }
+        [HttpGet]
+        public HttpResponseMessage deleteData(int id) {
+            taskDetail t = db.taskDetails.Where((i) => i.id == id).FirstOrDefault();
+            if (t != null)
+            {
+                db.taskDetails.Remove(t);
+                db.SaveChanges();
+                return Request.CreateResponse("Successfully Deleted");
+
+            }
+            else
+            {
+                return Request.CreateResponse("No id exist");
+            }
+            }
+
+
+    }
+}
